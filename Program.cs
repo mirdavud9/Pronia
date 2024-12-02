940ab61be63ece3fa41127e3859220b59e5cef5d
DevDavud <mirdavudvm-bp217@code.edu.az>
2024-12-02 23:48:28 +0400

    Update Program.cs
    random yoxlama

--- Program.cs
+++ Program.cs
@@ -5,9 +5,8 @@ namespace Pronia
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+            var app = builder.Build()
 
-            app.MapGet("/", () => "Hello World!");
 
             app.Run();
         }
