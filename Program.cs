7aaf5e35e79aa773d17c0c0f4531939b58191ba0
DevDavud <mirdavudvm-bp217@code.edu.az>
2024-12-02 23:50:39 +0400

    Update Program.cs
    hbhjhuhj

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace Pronia
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build()
+
 
 
             app.Run();
